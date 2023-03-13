2aa82e5bb696c5c4d8d5ff244a0a9f3018fd1905
Jegler <jtegler213@gmail.com>
2023-03-13 00:02:20 -0500

    changed the file structure

---
+++ index.js
@@ -0,0 +1,40 @@
+const jsondata = require("./data.json");
+var videos = jsondata;
+
+/*
+var render = function (data) {
+  thumbnail = data.map(function (video) {
+    return video.thumbnail;
+  });
+  return thumbnail;
+};
+console.log(render(videos));
+*/
+
+var info = function (data) {
+  var videosHTMLString =
+    "<ul>" +
+    data
+      .map(function (video) {
+        return (
+          "<li>" +
+          "<strong>Title: </strong>" +
+          video.title +
+          "<br/>" +
+          "<strong>Author: </strong>" +
+          video.creator +
+          "<br/>" +
+          "<strong>Category: </strong>" +
+          video.category +
+          "<br/>" +
+          "<strong>Release Date: </strong>" +
+          video.release +
+          "<br/>" +
+          "</li>"
+        );
+      })
+      .join("");
+  +"</ul>";
+  console.log(videosHTMLString);
+};
+info(videos);
