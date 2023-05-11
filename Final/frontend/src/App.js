24dea9fe6dd6c0d2132c15a3e95e9fc4cdc31e23
ZacharyKreutner <zakreu@iastate.edu>
2023-05-10 22:05:38 -0500

    More work on the frontend.

--- Final/frontend/src/App.js
+++ Final/frontend/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+src="https://unpkg.com/axios/dist/axios.min.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const weather='http://localhost:3000/weather';
+const history='http://localhost:3000/history'
 
-export default App;
+axios.get(weather)
+  .then(data=>console.log(data))
+  .catch(err=>console.log(err))
