81ce3d2d13651602430b960420a7520407bf791f
Jegler <jtegler213@gmail.com>
2023-05-12 16:48:39 -0500

    some changes

--- Final/frontend/src/App.js
+++ Final/frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import WeatherComponent from "./index";
+import getWeather from "./index";
+import getHistory from "./index";
 
 function App() {
   return (
     <div>
+      <button onClick={getWeather}>Current Weather</button>
+      <button onClick={getHistory}>Weather History</button>
       <WeatherComponent />
     </div>
   );
